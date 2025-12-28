ebddeda4b3743ba5ae809cacd1fa5e7a9d3fabc3
Arinze Ogbuniba <niklaus.oa@gmail.com>
2025-12-28 16:10:26 +0100

    feat: add rates ticker showcase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import RatesCarousel from "@/components/RatesCarousel";
 
 export default function Page() {
   return (
     <div>
       <Hero />
+      <RatesCarousel />
     </div>
   );
 }
